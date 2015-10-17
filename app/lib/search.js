899e070977f6541f5ea2dd36043495d8f76f1db8
Linus Oleander <linus@oleander.nu>
2015-10-17 15:28:11 +0200

    Select the best match from open subtitle

--- app/lib/search.js
+++ app/lib/search.js
@@ -4,30 +4,47 @@ var http = nRequire("http");
 var fs = nRequire("fs");
 var path = nRequire("path")
 var url = nRequire("url");
+var wuzzy = nRequire("wuzzy");
+
+var selectBestMatch = function(results, query){
+  var cleanUp = function(value) {
+    return value.replace(/[^a-z\d]/gi, "");
+  }
+  var searchQuery = cleanUp(query);
+  var distance = function(value){
+    return wuzzy.levenshtein(
+      cleanUp(value.MovieReleaseName), searchQuery
+    );
+  }
+  return results.sort(function(a, b){
+    return distance(b) - distance(a);
+  })[0];
+}
 
 var searchAndDownload = function(token, lang, query, file_path) {
   return new Promise(function(accept, reject){
     opensubtitles.api.searchForTitle(token, lang, query).then(function(results){
-      if(!results[0]) { return reject("No subtitles found"); }
-      if(results[0]){
-        var downloadUrl = results[0].SubDownloadLink
-        var p = path.dirname(file_path)
-        var u = url.parse(downloadUrl);
-        var request = http.get({ 
-            host: u.host,
-            path: u.path,
-            port: 80
-        });
+      if(!results.length) { return reject("No subtitles found"); }
+      var match = selectBestMatch(results, query);
+      if(!match) { return reject("No matching subtitle"); }
 
-        request.on("response", function(response) {
-          response.on("end", accept);
-          var fileData = path.parse(results[0].SubFileName)
-          var output = fs.createWriteStream(p + "/" + query + fileData.ext);
-          response.pipe(zlib.createGunzip()).pipe(output);
-        });
+      var downloadUrl = match.SubDownloadLink
+      var p = path.dirname(file_path)
+      var u = url.parse(downloadUrl);
+      var request = http.get({ 
+          host: u.host,
+          path: u.path,
+          port: 80
+      });
 
-        request.on("error", reject);
-      }
+      request.on("response", function(response) {
+        response.on("end", accept);
+        var fileData = path.parse(match.SubFileName)
+        var output = fs.createWriteStream(path.join(p, query + fileData.ext));
+        response.pipe(zlib.createGunzip()).pipe(output);
+      });
+
+      request.on("error", reject);
     }).fail(reject);
   })
 }
